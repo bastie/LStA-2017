648e3904c703e9483574543eb6de59ecc91aa59e
Sͬeͥbͭaͭsͤtͬian <Sebnospam@example.com>
2017-06-20 22:26:15 +0200

    Initial

---
+++ Lohnsteueranmeldung 2017/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Lohnsteueranmeldung 2017
+//
+//  Created by Sebastian Ritter on 20.06.17.
+//  Copyright © 2017 Sebastian Ritter. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
